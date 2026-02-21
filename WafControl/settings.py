48700e9a082c047f55ebbcd27b4c27c5117d316e
Omar Khalil <o@omkh.net>
2026-02-21 18:01:50 +0100

    fix: add CSRF_TRUSTED_ORIGINS env support and gitignore celery beat files

--- WafControl/settings.py
+++ WafControl/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
-
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 # Application definition
 
